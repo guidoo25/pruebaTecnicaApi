3582488196ed3a2bbb4c334380de037c48a9c9a7
guido gutierrez <121067148+guidoo25@users.noreply.github.com>
2024-01-24 12:41:59 -0500

    añado servicios de la actividad 3

---
+++ pruebaTecnicaApi/Services/BillboardServices.cs
@@ -0,0 +1,53 @@
+﻿using database;
+
+namespace pruebaTecnicaApi.Services
+{
+    public class BillboardServices
+    {
+        private readonly CineContext _context;
+
+        public BillboardServices( CineContext context ) { 
+        _context = context;
+        }
+        public async Task CancelBillboardAndAllBookings(int billboardId)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var billboard = await _context.BillboardEntities.FindAsync(billboardId);
+                    if (billboard.Date < DateTime.Now)
+                    {
+                        throw new Exception("No se puede cancelar funciones de la cartelera con fecha anterior a la actual");
+                    }
+
+                    billboard.Status = false;
+                    _context.BillboardEntities.Update(billboard);
+
+                    var bookings = _context.BookingEntities.Where(b => b.BillboardId == billboardId);
+                    foreach (var booking in bookings)
+                    {
+                        booking.Status = false;
+                        _context.BookingEntities.Update(booking);
+
+                        var seat = await _context.SeatEntities.FindAsync(booking.SeatId);
+                        seat.Status = true;
+                        _context.SeatEntities.Update(seat);
+
+                        Console.WriteLine($"Cliente alterado: {booking.CustomerId}");
+                    }
+
+                    await _context.SaveChangesAsync();
+
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
+
---
+++ pruebaTecnicaApi/Services/BookingService.cs
@@ -0,0 +1,40 @@
+﻿using database;
+
+namespace pruebaTecnicaApi.Services
+{
+    public class BookingService
+    {
+        private readonly CineContext _context;
+
+        public BookingService(CineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CancelBookingAndInactivateSeat(int bookingId, int seatId)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var seat = await _context.SeatEntities.FindAsync(seatId);
+                    seat.Status = false;
+                    _context.SeatEntities.Update(seat);
+
+                    var booking = await _context.BookingEntities.FindAsync(bookingId);
+                    booking.Status = false;
+                    _context.BookingEntities.Update(booking);
+
+                    await _context.SaveChangesAsync();
+
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
